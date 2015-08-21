06392a1eaa7a0ac1e9c655a731a60c6d64e54571
samuel silva pereira <samukapsilva@gmail.com>
2015-08-21 14:19:02 -0300

    Delete LancadorDeDado.java
    nao versionavel

--- dadoGWeb/src/br/com/devejb/sessionbeans/LancadorDeDado.java
+++
@@ -1,9 +0,0 @@
-package br.com.devejb.sessionbeans;
-
-import java.util.Map;
-import java.util.concurrent.Future;
-
-public interface LancadorDeDado {
-	int lanca ();
-	Future <Map<Integer,Integer>> calculaFrequencia();	
-}
